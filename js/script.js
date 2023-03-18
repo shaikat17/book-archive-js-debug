f1c2661c6b8af2ce97592e0f134190b9209a0c0e
shaikat17 <shaikatpal56@gmail.com>
2023-03-18 13:34:54 +0600

    working on data display

--- js/script.js
+++ js/script.js
@@ -15,29 +15,31 @@ const searchBook = () => {
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data)
+      // console.log(data.docs)
       displaySearchResult(data.docs)
     });
 };
 
 // ----------display search result data----------
 const displaySearchResult = (myBooks) => {
+  // console.log(myBooks)
   const searchResult = document.getElementById("search-result");
   searchResult.innerText = "";
 
 
-  const books = myBooks.docs;
+  const books = myBooks;
+  console.log(books)
   books.forEach((book) => {
     console.log(book);
-    const div = document.innerHTML("div");
-    div.classList.add("col");
+    const div = document.createElement("div");
+    div.classList.add("col-sm-1", "col-md-4");
     div.innerHTML = `
         <div class="card">
             <img src=" https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top w-50 mx-auto" alt="...">
             <div class="card-body text-center">
               <h5 class="card-title"> Name: ${book.title}</h5>
               <h6> Author: ${book.author_name}</h6>
-              <p>Publisher: <small> ${book.publisher[0]} </small></p>
+              <p>Publisher: ${book.publisher ? `<small>${book.publisher[0]}</small>` : 'Not Found'}</p>
               <small> First Published Year: ${book.first_publish_year}</small>
             </div>
         </div>
